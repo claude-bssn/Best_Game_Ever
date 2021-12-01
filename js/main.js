b1e0afcf3a983bc79e60c6010868bc290021acdf
adrien <adrien.charrier@outlook.fr>
2021-12-01 13:25:47 +0100

    ADD - assets

--- js/main.js
+++ js/main.js
@@ -10,15 +10,27 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
+const tileSize = 16;
+const tileResize = 2
+var numRows = 4;
+var numCols = 5;
 
 function preload ()
 {
+    this.load.spritesheet('assets', 'public/terrain/Assets.png', {
+        frameWidth: tileSize,
+        frameHeight: tileSize
+    })
 }
 
 function create ()
 {
+    for (let i = 0; i * tileSize * tileResize < config.width; i++) {
+        var sprite = this.add.image( i * tileSize * tileResize + tileSize * tileResize / 2, config.height-tileSize * tileResize/2, 'assets', 4).setScale(2);
+    }
 }
 
 function update ()
 {
+    
 }
