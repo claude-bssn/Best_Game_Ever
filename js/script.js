3c89156ed1cec47813ef4563dd5c9a08e58681f0
admin <admin@iMac-ITA-11.local>
2021-12-01 14:36:24 +0100

    create function

--- js/script.js
+++ js/script.js
@@ -21,52 +21,55 @@ function preload ()
 
 function create ()
 {
-
+    this.add.image(100,50,'bottle');
   bottle = this.physics.add.group({
       key: 'bottle',
-      repeat: 1,
-      setXY: { x: 12, y: 0}
+      repeat: 5,
+      setXY: { x: 1200, y: 0, stepX: 70 }
   });
 
-  bottle.children.iterate(function (child) {
+//   bottle.children.iterate(function (child) {
 
-      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+//       child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
 
-  });
+//   });
 
-  kerosen = this.physics.add.group({
-      key: 'kerosen',
-      repeat: 1,
-      setXY: { x: 12, y: 0, stepX: 70 }
-  });
+//   kerosen = this.physics.add.group({
+//       key: 'kerosen',
+//       repeat: 1,
+//       setXY: { x: 12, y: 0, stepX: 70 }
+//   });
 
-  kerosen.children.iterate(function (child) {
+//   kerosen.children.iterate(function (child) {
 
-      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-  });
+//       child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+//   });
 
-  lighter = this.physics.add.group({
-      key: 'lighter',
-      repeat: 1,
-      setXY: { x: 12, y: 0, stepX: 70 }
-  });
+//   lighter = this.physics.add.group({
+//       key: 'lighter',
+//       repeat: 1,
+//       setXY: { x: 12, y: 0, stepX: 70 }
+//   });
 
-  lighter.children.iterate(function (child) {
-      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-  });
+//   lighter.children.iterate(function (child) {
+//       child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+//   });
 
-  tissue = this.physics.add.group({
-      key: 'tissue',
-      repeat: 1,
-      setXY: { x: 12, y: 0, stepX: 70 }
-  });
+//   tissue = this.physics.add.group({
+//       key: 'tissue',
+//       repeat: 1,
+//       setXY: { x: 12, y: 0, stepX: 70 }
+//   });
 
 
-  tissue.children.iterate(function (child) {
+//   tissue.children.iterate(function (child) {
 
-      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+//       child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
 
-  });
+//   });
+
+
+
 }
 
 function update ()
